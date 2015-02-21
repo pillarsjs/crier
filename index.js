51db498eb084ddc65418fe496261ae1db874774e
Bifuer <bifuer@odeux.com>
2015-02-21 15:19:39 +0100

    packaging...

---
+++ index.js
@@ -0,0 +1,123 @@
+/* jslint node: true */
+"use strict";
+
+var ObjectArray = require('objectarray');
+var Procedure = require('procedure');
+var colors = require('colors');
+require('json.decycled');
+require('date.format');
+
+module.exports = new Crier();
+function Crier(parent,id){
+  this.id = id;
+  this.parent = parent;
+}
+  Crier.prototype.addLvl = function(lvl){
+    Crier.prototype[lvl] = function crierLvl(msg,meta,callback){
+      this.$compose([],lvl,msg,meta,callback);
+      return this;
+    };
+    return this;
+  };
+  Crier.prototype.getLvls = function(){
+    var lvls = [];
+    for(var i=0,k=Object.keys(Crier.prototype),l=k.length;i<l;i++){
+      if(Crier.prototype[k[i]].name === 'crierLvl'){
+        lvls.push(k[i]);
+      }
+    }
+    return lvls;
+  };
+  Crier.prototype.addGroup = function(id){
+    if(!this[id]){
+      this[id] = new Crier(this,id);
+    }
+    return this[id];
+  };
+  Crier.prototype.getGroups = function(){
+    var groups = [];
+    for(var i=0,k=Object.keys(this),l=k.length;i<l;i++){
+      if(this[k[i]] instanceof Crier){
+        groups.push(k[i]);
+      }
+    }
+    return groups;
+  };
+  Crier.prototype.$compose = function(location,lvl,msg,meta,callback){
+    if(this.parent){
+      location = location || [];
+      location.push(this.id);
+      this.parent.$compose(location,lvl,msg,meta,callback);
+    } else {
+      location = location || [];
+      location = location.reverse();
+      msg = msg || '';
+      meta = meta || {};
+
+      var stores = [];
+      for(var i=0,l=Crier.rules.length;i<l;i++){
+        Crier.rules[i].rule(stores,location,lvl,msg,meta);
+      }
+      if(stores.length===0){stores=['console'];}
+
+      var procedure = new Procedure();
+      for(i=0,l=stores.length;i<l;i++){
+        var store = Crier.stores.get(stores[i]);
+        if(store){
+          procedure.add(store.handler.bind(store),location,lvl,msg,meta);
+        }
+      }
+      procedure.race().launch(function(error){
+        var args = Array.prototype.slice.call(arguments);
+        if(callback){
+          callback.apply(this,args);
+        }
+      });
+    }
+  };
+
+
+  Crier.prototype.log = function(msg,meta,callback){
+    this.$compose([],'log',msg,meta,callback);
+  };
+  Crier.prototype.info = function(msg,meta,callback){
+    this.$compose([],'info',msg,meta,callback);
+  };
+  Crier.prototype.alert = function(msg,meta,callback){
+    this.$compose([],'alert',msg,meta,callback);
+  };
+  Crier.prototype.error = function(msg,meta,callback){
+    this.$compose([],'error',msg,meta,callback);
+  };
+  Crier.prototype.warn = function(msg,meta,callback){
+    this.$compose([],'warn',msg,meta,callback);
+  };
+
+  
+  Crier.console = {
+    id: 'console',
+    language: 'en',
+    colors: {log:'cyan',info:'green',alert:'yellow',error:'bgRed',warn:'bgYellow'},
+    format: function(text,meta,lang){return text;},
+    handler: function(groups,lvl,msg,meta,callback){
+      var node = groups.join('.')+'.'+msg;
+      var format = this.format(node,meta,this.language);
+      var timestamp = (new Date()).format('{YYYY}/{MM}/{DD} {hh}:{mm}:{ss}',true).grey;
+      var output = timestamp;
+      if(format===node){
+        output += (' '+lvl.toUpperCase()+'.'+groups.join('.')+': ')[(this.colors[lvl]?this.colors[lvl]:'white')];
+        output += msg;
+        output += '\n'+JSON.decycled(meta).replace(/(\\n|\\r)/g,'\n').replace(/\\t/g,'\t');
+        if(meta.error && meta.error.stack){
+          output += ('\n\n'+meta.error.stack+'\n').bgRed;
+        }
+        output += '\n';
+      } else {
+        output += format;
+      }
+      console.log(output);
+      callback(undefined,output);
+    }
+  };
+  Crier.rules = new ObjectArray();
+  Crier.stores = new ObjectArray(Crier.console);
